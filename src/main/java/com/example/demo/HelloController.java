e4bc33e55d58d77d24491c6125a1e1c08e1c58a5
TianXiang <18569069613@163.com>
2018-12-06 14:11:12 +0800

    上传

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @version v1.0
+ * @author: TianXiang
+ * @description:
+ * @date: 2018/12/6
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "SpringBoot, Hello~";
+    }
+}
